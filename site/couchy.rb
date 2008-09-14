91e27f4a88c1929d4e10fe9eb11a07f85a5ca3c5
Paul Campbell <paul@rslw.com>
2008-09-15 00:39:49 +0100

    basic update

--- site/couchy.rb
+++ site/couchy.rb
@@ -17,7 +17,19 @@ get '/pages' do
   erb :pages
 end
 
-get '/editor/:id' do
+get '/edit/:id' do
   @page = OpenStruct.new(db.get(params[:id]))
-  erb :editor
+  erb :edit
+end
+
+post '/page/:id' do
+  page = OpenStruct.new(db.get(params[:id]))
+  @page = db.save({"_id" => params[:id], "_rev" => page._rev, 'title' => params[:title], 'body' => params[:body]})
+  redirect '/pages'
+end
+
+get '/delete/:id' do
+  @page = db.get(params[:id])
+  db.delete(@page)
+  redirect '/pages'
 end
